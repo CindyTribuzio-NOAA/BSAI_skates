0692f6895688b14b334cb46a90602043e09e18ad
CindyTribuzio-NOAA <cindy.tribuzio@noaa.gov>
2025-08-14 09:58:05 -0800

    Added vers/bias and jitter runs to decomposition

---
+++ 2025/2025_Sept_models/AK_skate_Tier3/kitchen_sink_decomposed.R
@@ -0,0 +1,38 @@
+# Breaking down the various component of the Kitchen Sink model
+# individual changes
+# developed by C Tribuzio August 2025
+
+# setup ----
+libs <- c("r4ss", "here", "tidyverse")
+if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {
+  install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
+lapply(libs, library, character.only = TRUE)
+'%nin%'<-Negate('%in%') #this is a handy function
+
+# Default with no version downloads the latest release
+# r4ss::get_ss3_exe()
+
+exe_loc <- here::here('2025/2025_Sept_models/AK_skate_Tier3/ss.exe')
+
+# Model 14_2d + bias correction and updated SS3 version----
+M142_v_bias_path <- here::here('2025/2025_Sept_models/AK_skate_Tier3/kitchen_sink_decompose/M14_2d_vers_bias')
+
+r4ss::run(dir = M142_v_bias_path, skipfinished = FALSE, exe = exe_loc)
+
+M142_v_bias_out <- SS_output(M142_v_bias_path, printstats = FALSE, verbose = FALSE)
+
+# plots the results
+SS_plots(M142_v_bias_out)
+
+# Model 14_2d + vers/bias and jitter/lamda----
+# jitter changed from 0.01 to 0.1 in starter file
+# #_maxlambdaphase from 4 to 1 in control file
+# sd_offset from 1 to 0 in control file
+M142_jitter <- here::here('2025/2025_Sept_models/AK_skate_Tier3/kitchen_sink_decompose/M14_2d_vb_jitter_lambda')
+
+r4ss::run(dir = M142_jitter, skipfinished = FALSE, exe = exe_loc)
+
+M142_jitter_out <- SS_output(M142_jitter, printstats = FALSE, verbose = FALSE)
+
+# plots the results
+SS_plots(M142_jitter_out)
